435b384ccd8b90077469919aba0422aecd362ccd
Jamie Tartell <jetartell@gmail.com>
2020-01-31 03:29:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -91,4 +91,12 @@ function makeInt(a) {
   return parseInt(a,10)
 }
 
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
 
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
+})

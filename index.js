5e57badda9a43a92cd32ae359307c2bf82d04cae
Jamie Tartell <jetartell@gmail.com>
2020-01-31 03:27:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -82,6 +82,11 @@ function makeInt(a) {
   return parseInt('a');
 }
 
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+
 function makeInt(a) {
   return parseInt(a,10)
 }
